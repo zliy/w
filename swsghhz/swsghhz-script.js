842df851b15b1b75c8cb091861fdf307c3a48962
zliy <zhuliy@outlook.com>
2017-03-01 21:35:25 +0800

    try

--- swsghhz/swsghhz-script.js
+++ swsghhz/swsghhz-script.js
@@ -1,6 +1,8 @@
-var map = new BMap.Map("container");
+var map = new BMap.Map("BMapContainer");
 map.centerAndZoom(new BMap.Point(106, 37), 5);
-map.addControl(new BMap.NavigationControl({type: BMAP_NAVIGATION_CONTROL_SMALL}));
+map.addControl(new BMap.NavigationControl({
+    type: BMAP_NAVIGATION_CONTROL_SMALL
+}));
 map.enableScrollWheelZoom();
 
 var drawedLayers = [];
@@ -10,7 +12,10 @@ function drawCurrentEditing() {
     var name = document.getElementById("districtName").value;
     var style = new LayerStyle();
     style.readSytleFromHtml();
-    drawedLayers.push({name: name, style: style});
+    drawedLayers.push({
+        name: name,
+        style: style
+    });
     var a = document.getElementById('setview').checked;
     drawByCityName(name, style, a);
 }
@@ -20,17 +25,17 @@ function drawByCityName(cityName, layerStyle, isSetview) {
 
 
     var bdary = new BMap.Boundary();
-    bdary.get(cityName, function (rs) {       //获取行政区域
-                                              //map.clearOverlays();        //清除地图覆盖物
+    bdary.get(cityName, function (rs) { //获取行政区域
+        //map.clearOverlays();        //清除地图覆盖物
         var count = rs.boundaries.length; //行政区域的点有多少个
         if (!rs.boundaries.length) {
             alert('未找到地区名')
         }
         for (var i = 0; i < count; i++) {
             var ply = new BMap.Polygon(rs.boundaries[i], layerStyle); //建立多边形覆盖物
-            map.addOverlay(ply);  //添加覆盖物
+            map.addOverlay(ply); //添加覆盖物
             if (isSetview) {
-                map.setViewport(ply.getPath());    //调整视野
+                map.setViewport(ply.getPath()); //调整视野
 
             }
         }
@@ -47,8 +52,8 @@ function delLastLayer() {
 
         drawByCityName(d.name, d.style, false);
     }
-    var saveLastName=drawedLayers[drawedLayers.length-1].name;
-    document.getElementById('districtName').value =saveLastName;
+    var saveLastName = drawedLayers[drawedLayers.length - 1].name;
+    document.getElementById('districtName').value = saveLastName;
     drawedLayers.pop();
 
 }
@@ -62,19 +67,21 @@ function enter(e) {
 }
 
 function showbSize() {
-    document.getElementById('bsizenum').innerHTML=document.getElementById("bSize").value
+    document.getElementById('bsizenum').innerHTML = document.getElementById("bSize").value
 }
 
 function LayerStyle(bColor,
-                    fColor,
-                    bWeight,
-                    bOpacity,
-                    fOpacity) {
+    fColor,
+    bWeight,
+    bOpacity,
+    fOpacity,
+    strokeStyle) {
     this.strokeColor = bColor;
     this.fillColor = fColor;
     this.strokeWeight = bWeight;
     this.strokeOpacity = bOpacity;
     this.fillOpacity = fOpacity;
+    this.strokeStyle = strokeStyle
 
     this.readSytleFromHtml = function () {
         var bColor = document.getElementById("bColor").value;
@@ -88,13 +95,15 @@ function LayerStyle(bColor,
             fColor = "";
         }
         var bWeight = parseInt(document.getElementById("bSize").value);
-
-
+        var strokeStyle = document.getElementById("strokeStyle").checked ? "dashed" : "solid"
+        // debugger
         this.strokeColor = bColor;
         this.fillColor = fColor;
         this.strokeWeight = bWeight;
         this.strokeOpacity = bOpacity;
         this.fillOpacity = fOpacity;
+        this.strokeStyle = strokeStyle
+
 
     }
 }
@@ -103,8 +112,8 @@ function drawLayer(points, layerStyle) {
 
     for (var i = 0; i < count; i++) {
         var ply = new BMap.Polygon(rs.boundaries[i], style); //建立多边形覆盖物
-        map.addOverlay(ply);  //添加覆盖物
-        map.setViewport(ply.getPath());    //调整视野
+        map.addOverlay(ply); //添加覆盖物
+        map.setViewport(ply.getPath()); //调整视野
     }
 
 }
@@ -113,4 +122,3 @@ function location_Points(locationName, points) {
     this.locationName = locationName;
     this.points = points;
 }
-
